8747a73f16b8852eec9bd8365b2586f18c2af1f5
Aleksefo <aleksefo@gmail.com>
2020-06-17 15:19:02 +0300

    Add Config.ts

---
+++ src/values/Config.ts
@@ -0,0 +1,7 @@
+const Config = {
+  redditURL: 'https://amp.reddit.com/',
+  subRedditName: 'r/ClimateActionPlan/',
+  movieDB: 'https://api.themoviedb.org/3/movie/popular',
+}
+
+export default Config
