904f8f5075da84e1b2146edfe7d2a671fd90ddd5
Aleksefo <aleksefo@gmail.com>
2020-06-17 15:14:00 +0300

    Add Colors.ts

---
+++ src/values/Colors.ts
@@ -0,0 +1,18 @@
+export default {
+  primary: '#7f00ff', // Port Gore
+  accent: '#ffff01', // Carrot Orange
+  error: '#D2473C',
+  success: '#40e6b3',
+  text: '#000000',
+  grey1: '#edeced',
+  grey2: '#c5c9cb', //inactiveLight
+  grey3: '#555a5e',
+  grey4: '#333537',
+  background: '#ffffff',
+  backgroundSet1: '#b4e7be',
+  backgroundSet2: '#f8f9b6',
+  backgroundSet3: '#f5afb9',
+  backgroundSet4: '#9ec1f7',
+  backgroundPaused: '#e6e6e5',
+  white: '#ffffff',
+}
