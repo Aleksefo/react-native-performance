4256ced87d1c2b19a3721352c40e002c1e0ea89c
Aleksefo <aleksefo@gmail.com>
2020-06-18 22:35:18 +0300

    Add Typography

---
+++ src/values/Typography.ts
@@ -0,0 +1,34 @@
+export default {
+  h1: {
+    fontSize: 48,
+    lineHeight: 56,
+    fontWeight: 'bold',
+  },
+  h2: {
+    fontSize: 28,
+    lineHeight: 36,
+    fontWeight: 'bold',
+  },
+  h3: {
+    fontSize: 24,
+    lineHeight: 32,
+  },
+  h4: {
+    fontSize: 20,
+    lineHeight: 24,
+  },
+  h5: {
+    fontSize: 16,
+    lineHeight: 24,
+  },
+  body: {
+    fontSize: 16,
+  },
+  bodyLarge: {
+    fontSize: 20,
+  },
+  bodySmall: {
+    fontSize: 12,
+    lineHeight: 16,
+  },
+}
