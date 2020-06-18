951e07225553d1a4320fe862677cbcc50f878f76
Aleksefo <aleksefo@gmail.com>
2020-06-18 22:35:39 +0300

    Add Strings.ts

---
+++ src/values/Strings.ts
@@ -0,0 +1,10 @@
+export default {
+  // Movie List Screen
+  movieListScreen: 'Movies list',
+  movieDetailsScreen: 'Movie details',
+  originalLanguage: 'Original language:',
+  releaseDate: 'Release date:',
+  score: 'score:',
+  overview: 'Overview:',
+  trailers: 'Trailers:',
+}
